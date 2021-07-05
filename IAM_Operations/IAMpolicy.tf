508a78331fdd95e6befe33470df4753f9f42a161
jatin-wipro <66778854+jatin-wipro@users.noreply.github.com>
2021-07-05 16:51:18 +0530

    Update IAMpolicy.tf

--- IAM_Operations/IAMpolicy.tf
+++ IAM_Operations/IAMpolicy.tf
@@ -1,1 +1,10 @@
+#add multiple iam user
+variable "username" {
+  type = "list"
+  default = ["prashant","pankaj","ashish"]
+}
 
+resource "aws_iam_user" "example" {
+  count = "${length(var.username)}"
+  name = "${element(var.username,count.index )}"
+}
